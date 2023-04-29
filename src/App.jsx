baa016a277ef157b399e3d69ed881d79784e48d0
“Edward <eminaya49@gmail.com>
2023-04-29 14:20:44 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// Components
+import { Header } from "./Header";
+import { Content } from "./Content";
+import { Footer } from "./Footer";
+
+// Styling
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <header>
+      <nav>
+        <a href="#">Home</a> | <a href="#">About</a> | <a href="#">My Todos</a>
+      </nav>
+    </header>
+  );
+}
